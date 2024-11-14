f89ed6e78ff4532fb01b9d88ea59d6addfcdc9f2
arpittiwari24 <atofficial2410@gmail.com>
2024-11-14 23:00:56 +0530

    seo added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,30 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "PenAI | AI GitHub Readme Generator",
   description: "PenAI is an AI GitHub Readme Generator that helps you create a beautiful README.md file for your GitHub repository.",
+  twitter: {
+    card: "summary_large_image",
+    site: "@arrpitttwts",
+    creator: "@arrpitttwts",
+    title: "PenAI | AI GitHub Readme Generator",
+    description: "PenAI is an AI GitHub Readme Generator that helps you create a beautiful README.md file for your GitHub repository.",
+    images: "https://penai.arrpit.work/og-image.png",
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://penai.arrpit.work",
+    siteName: "PenAI | AI GitHub Readme Generator",
+    title: "PenAI | AI GitHub Readme Generator",
+    description: "PenAI is an AI GitHub Readme Generator that helps you create a beautiful README.md file for your GitHub repository.",
+    images: [
+      {
+        url: "https://penai.arrpit.work/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "PenAI | AI GitHub Readme Generators",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
