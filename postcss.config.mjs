7c2485d16de2999764eac816f6a150fb5c444c8f
arpittiwari24 <atofficial2410@gmail.com>
2024-11-17 19:08:41 +0530

    njkfghruy

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
+import tailwindcss from 'tailwindcss';
+import renameClass from 'postcss-rename-class';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
-    require('tailwindcss'),
-    require('postcss-rename-class')({
+    tailwindcss,
+    renameClass({
       rename: (className) => `obf-${className}-${Math.random().toString(36).substr(2, 5)}`,
     }),
   ],
