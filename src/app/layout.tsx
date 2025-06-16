01c21d8095eca6ed0d4749bf6cc73eb238e05141
arpittiwari24 <atofficial2410@gmail.com>
2025-06-16 16:43:59 +0530

    fix: format metadata for improved readability and consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Free AI GitHub README Generator - Make Your Repo Stand Out",
-  description: "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
-  keywords : "Readme Generator, AI, GitHub, Readme, Generator, AI GitHub Readme Generator, GitHub Readme Generator, readme file generator, AI Readme Generator , PenAI Readme Generator , git readme generator , readme ai", 
+  description:
+    "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
+  keywords:
+    "Readme Generator, AI, GitHub, Readme, Generator, AI GitHub Readme Generator, GitHub Readme Generator, readme file generator, AI Readme Generator , PenAI Readme Generator , git readme generator , readme ai",
   twitter: {
     card: "summary_large_image",
     site: "@arrpitttwts",
     creator: "@arrpitttwts",
     title: "AI GitHub Readme Generator",
-    description: "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
+    description:
+      "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
     images: "https://readme-generator.xyz/og.png",
   },
   openGraph: {
@@ -24,7 +27,8 @@ export const metadata: Metadata = {
     url: "https://readme-generator.xyz",
     siteName: "AI GitHub Readme Generator",
     title: "Free AI GitHub README Generator - Make Your Repo Stand Out",
-    description: "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
+    description:
+      "Create stunning and professional README.md files for your GitHub repos using AI. Fast, easy, and beautiful. Try it in seconds!",
     images: [
       {
         url: "https://readme-generator.xyz/og.png",
@@ -44,43 +48,59 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="19d2ee6f-c216-4382-be58-bdeeb27f797b"></script>
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
-  "@context": "https://schema.org",
-  "@type": "SoftwareApplication",
-  "name": "AI GitHub Readme Generator",
-  "url": "https://readme-generator.xyz",
-  "applicationCategory": "DeveloperApplication",
-  "operatingSystem": "All",
-  "description": "Readme Generator is an AI-powered tool that helps you generate beautiful and professional README.md files for your GitHub projects.",
-  "keywords": [
-    "AI Readme Generator",
-    "GitHub Readme Generator",
-    "Readme Generator",
-    "Git Readme Generator",
-    "Free Readme Generator"
-  ],
-  "creator": {
-    "@type": "Person",
-    "name": "Arrpit Tiwari"
-  },
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "5",
-    "reviewCount": "25"
-  },
-  "offers": {
-    "@type": "Offer",
-    "price": "0",
-    "priceCurrency": "USD"
-  }
-}) }} />
-
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="19d2ee6f-c216-4382-be58-bdeeb27f797b"
+        ></script>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "SoftwareApplication",
+              name: "AI GitHub Readme Generator",
+              url: "https://readme-generator.xyz",
+              applicationCategory: "DeveloperApplication",
+              operatingSystem: "All",
+              description:
+                "Readme Generator is an AI-powered tool that helps you generate beautiful and professional README.md files for your GitHub projects.",
+              keywords: [
+                "AI Readme Generator",
+                "GitHub Readme Generator",
+                "Readme Generator",
+                "Git Readme Generator",
+                "Free Readme Generator",
+              ],
+              creator: {
+                "@type": "Person",
+                name: "Arrpit Tiwari",
+              },
+              aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "5",
+                reviewCount: "25",
+              },
+              offers: {
+                "@type": "Offer",
+                price: "0",
+                priceCurrency: "USD",
+              },
+            }),
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              'window.lemonSqueezyAffiliateConfig = { store: "readme-gen" };',
+          }}
+        ></script>
+        <script src="https://lmsqueezy.com/affiliate.js" defer></script>
       </head>
       <Providers>
-      <body className={inter.className}>
-        <Toaster />
-        {children}
+        <body className={inter.className}>
+          <Toaster />
+          {children}
         </body>
       </Providers>
     </html>
